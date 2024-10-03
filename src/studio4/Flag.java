5211b3f27738c233788a610a2b976b7096e37cc1
zacharynowacek <zacharynowacek@localhost>
2024-10-03 11:16:12 -0500

    we done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,58 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		boolean toggle = true;
+		double[] xt = {0.4,0.5,0.45};
+		double [] yt = {0.6,0.6,0.05};
+		double[] x = {0.1,0.12,0.35};
+		double [] y = {0.85,0.9,0.75};
+		double [] x2 = {0.9,0.88,0.65};
+		double[] xt2 = {0.5,0.6,0.55};
+		double[] xn = {0.1,0.12,0.35};
+		double [] yn = {0.85,0.9,0.88};
+		double [] ynt = {0.6,0.6,0.55};
+		double [] xn2 = {0.9,0.88,0.65};
+		while(true) {
+			if(toggle==true) {
+				StdDraw.setPenColor(Color.BLUE);
+				StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.5);
+				StdDraw.setPenColor(Color.RED);
+				StdDraw.filledEllipse(0.5, 0.3, 0.5, 0.3);
+				StdDraw.setPenColor(Color.GREEN);
+				StdDraw.filledEllipse(0.8, 0.7, 0.1, 0.1);
+				StdDraw.filledEllipse(0.2, 0.7, 0.1, 0.1);
+				StdDraw.setPenColor(Color.YELLOW);
+				StdDraw.filledEllipse(0.8, 0.7, 0.01, 0.01);
+				StdDraw.filledEllipse(0.2, 0.7, 0.01, 0.01);
+				StdDraw.setPenColor(255, 0, 255);
+				StdDraw.filledPolygon(x,y);
+				StdDraw.filledPolygon(x2,y);
+				StdDraw.setPenColor(Color.WHITE);
+				StdDraw.filledPolygon(xt,yt);
+				StdDraw.filledPolygon(xt2,yt);
+			} else {
+				StdDraw.setPenColor(Color.BLUE);
+				StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.5);
+				StdDraw.setPenColor(Color.RED);
+				StdDraw.filledEllipse(0.5, 0.3, 0.5, 0.3);
+				StdDraw.setPenColor(Color.GREEN);
+				StdDraw.filledEllipse(0.8, 0.7, 0.1, 0.1);
+				StdDraw.filledEllipse(0.2, 0.7, 0.1, 0.1);
+				StdDraw.setPenColor(Color.YELLOW);
+				StdDraw.filledEllipse(0.8, 0.7, 0.01, 0.01);
+				StdDraw.filledEllipse(0.2, 0.7, 0.01, 0.01);
+				StdDraw.setPenColor(Color.WHITE);
+				StdDraw.filledPolygon(xt,ynt);
+				StdDraw.filledPolygon(xt2,ynt);
+				StdDraw.setPenColor(255, 0, 255);
+				StdDraw.filledPolygon(xn,yn);
+				StdDraw.filledPolygon(xn2,yn);
+			}
+			if(StdDraw.isMousePressed()) {
+				toggle = !toggle;
+				StdDraw.clear();
+			}
+			StdDraw.pause(1000);
+		}
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,32 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		StdDraw.setPenColor(red,green,blue);
+		if(shape.equals("ellipse")) {
+			if(filled) {
+				StdDraw.filledEllipse(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			} else {
+				StdDraw.ellipse(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+		} else if(shape.equals("rectangle")) {
+			if(filled) {
+				StdDraw.filledRectangle(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			} else {
+				StdDraw.rectangle(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+		} else {
+			double [] x = {in.nextDouble(), in.nextDouble(), in.nextDouble()};
+			double [] y = {in.nextDouble(), in.nextDouble(), in.nextDouble()};
+			if(filled) {
+				StdDraw.filledPolygon(x,y);
+			} else {
+				StdDraw.polygon(x,y);
+			}
+		}
 	}
 }
